9a723a85751bb176ca8d4a5479cc169f5ea3ecec
Кирилл <74520222+kirilliv02@users.noreply.github.com>
2021-05-06 21:56:29 +0300

    06.05

---
+++ HomeWork-06_05/src/Product.java
@@ -0,0 +1,17 @@
+import java.io.Serializable;
+
+public class Product implements Serializable {
+    String name;
+    int count;
+
+    public Product(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + count;
+    }
+
+}
---
+++ HomeWork-06_05/src/ProductsListener.java
@@ -0,0 +1,3 @@
+public interface ProductsListener {
+    void productsUpdated();
+}
